3fab386512338939188f3e3e38a6a32a811a5b88
CxIvanxH <walkin_flash@hotmail.com>
2017-05-13 19:00:49 -0500

    Practica 4

---
+++ javascript.js
@@ -0,0 +1,33 @@
+  
+function resta(){
+  
+     var n1 = Number(document.getElementById('no1').value)
+     var n2 = Number(document.getElementById('no2').value)
+          alert("resultado  "+(n1-n2)); 
+
+ }
+ function suma(){
+  
+    var n1 = Number(document.getElementById('no1').value)
+     var n2 = Number(document.getElementById('no2').value)
+    var sum=n1+n2;
+     var expresion;
+     
+      alert("resultado  "+(n1+n2));
+    }
+function dividir(){
+  
+     var n1 = Number(document.getElementById('no1').value)
+     var n2 = Number(document.getElementById('no2').value)
+   
+          alert("resultado  "+(n1/n2));     
+
+ }
+function multiplicar(){
+  
+      var n1 = Number(document.getElementById('no1').value)
+     var n2 = Number(document.getElementById('no2').value)
+   
+          alert("resultado  "+(n1*n2)); 
+    
+}
